6b94ec0efc32f49b8e26fdd37f61cd812dfa10ed
Andrew Rost <andrewrost0@gmail.com>
2021-05-21 15:10:09 -0400

    basic routing, homepage

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import Header from "./common/Header";
+
+export default function App() {
+  function getPage() {
+    const route = window.location.pathname;
+    console.log(route);
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
+}
