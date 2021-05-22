7726f1c04b18c7a8618db861f7500681e10f61c8
Andrew Rost <andrewrost0@gmail.com>
2021-05-22 15:16:45 -0400

    intro to props, states

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
 
 export default function App() {
   function getPage() {
     const route = window.location.pathname;
-    console.log(route);
     if (route === "/about") return <AboutPage />;
+    if (route === "/courses") return <CoursesPage />;
     return <HomePage />;
   }
   return (
